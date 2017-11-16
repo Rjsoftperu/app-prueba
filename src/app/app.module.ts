d7db38518101f17721ea2e572a3c6aec978fec77
unknown <DESARROLLO30@DESARROLLO30.solmit.net>
2017-11-16 15:01:56 -0500

    canbios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { RepositoryComponent } from './repository/repository.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepositoryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
